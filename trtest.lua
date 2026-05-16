6173f6559a7d8307ffaad6763fd2a8a33d9bd8e7
Josue Arias <31876967+synthi@users.noreply.github.com>
2026-05-16 02:47:54 -0300

    Create trtest.lua

---
+++ trtest.lua
@@ -0,0 +1,13 @@
+engine.name = 'Transmissor'
+
+function init()
+  print("TEST INIT")
+end
+
+function redraw()
+  screen.clear()
+  screen.level(15)
+  screen.move(0, 32)
+  screen.text("ALIVE " .. os.clock())
+  screen.update()
+end
